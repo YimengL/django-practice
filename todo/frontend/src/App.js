c287bed68270d325e5826b0c871638b33bdf4fad
YimengL <yimengcsu@gmail.com>
2020-03-02 10:18:48 -0600

    use axios to get the data instead of using the hardcoded data

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+// src/App.js
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const list = [
-  {
-    "id":1,
-    "title":"1st todo",
-    "body":"Learn Django properly."
-  },
-]
+
+class App extends Component {
+  state = {
+    todos: []
+  };
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos() {
+    axios
+    .get('http://127.0.0.1:8000/api')
+    .then(res => {
+      this.setState({ todos: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.todos.map(item => (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
