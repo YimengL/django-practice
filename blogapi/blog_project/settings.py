c23f8949505760b95f39c4277a12c2e6e854de1a
YimengL <yimengcsu@gmail.com>
2020-03-05 16:38:21 -0600

    use default setting(basic auth + session based), explicitly

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -49,7 +49,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    ],
 }
 
 MIDDLEWARE = [
