2242a8d241d79b5e8f81637e3dc5d9b35f5de829
YimengL <yimengcsu@gmail.com>
2020-04-16 21:25:23 -0500

    Update settings.py

--- blog/blog_project/settings.py
+++ blog/blog_project/settings.py
@@ -32,9 +32,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
