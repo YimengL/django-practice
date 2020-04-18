0861589a28d63423d64cae15ddcbb92de698c63f
YimengL <yimengcsu@gmail.com>
2020-04-18 18:23:16 -0500

    Add signup, login, logout feature

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'newspaper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,3 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
