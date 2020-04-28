2b40dcec930d008eba74e2f55dc36c9e960bcdec
YimengL <yimengcsu@gmail.com>
2020-04-28 17:11:10 -0500

    rollback to the console based email message

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -122,3 +122,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'apikey'
+# EMAIL_HOST_PASSWORD = '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'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
